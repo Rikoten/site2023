a33901aeb276ef784c254f7d8d6e44cf311ebecc
saetakashiro <saetakashiro@gmail.com>
2023-09-10 20:07:28 +0900

    完成してない

--- js/lastyear.js
+++ js/lastyear.js
@@ -1,53 +1,14 @@
-$('.slider').slick({
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 3,//スライドを画面に3枚見せる
-    slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    dots: true,//下部ドットナビゲーションの表示
-    responsive: [
-      {
-      breakpoint: 769,//モニターの横幅が769px以下の見せ方
-      settings: {
-        slidesToShow: 2,//スライドを画面に2枚見せる
-        slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
-      }
-    },
-    {
-      breakpoint: 426,//モニターの横幅が426px以下の見せ方
-      settings: {
-        slidesToShow: 1,//スライドを画面に1枚見せる
-        slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-      }
-    }
-  ]
-  });
-
-
-
-
-
-  
-
-  const slider = document.querySelector(".slider");
-const prevButton = document.querySelector(".prev-button");
-const nextButton = document.querySelector(".next-button");
-
-let slideIndex = 0;
-
-prevButton.addEventListener("click", () => {
-  slideIndex = (slideIndex - 1 + slider.children.length) % slider.children.length;
-  updateSliderPosition();
+$(function(){
+  $(".slideshow-slide li").css({"position":"relative"});
+  $(".slideshow-slide li").hide().css({"position":"absolute"});
+  $(".slideshow-slide li:first").addClass("slide");
+  $(".slideshow-slide li:nth-child(2)").css({"display":"block"});
+  setInterval(function(){
+    var $active = $(".slideshow-slide li.slide");
+    var $next = $active.next("li").length?$active.next("li"):$(".slideshow-slide li:first");
+    var $nextnext = $next.next("li");
+    $active.fadeOut(0).removeClass("slide");
+    $next.show().addClass("slide");
+    $nextnext.css({"display":"block"});
+  },3000);
 });
-
-nextButton.addEventListener("click", () => {
-  slideIndex = (slideIndex + 1) % slider.children.length;
-  updateSliderPosition();
-});
-
-function updateSliderPosition() {
-  const slideWidth = slider.children[0].clientWidth;
-  const offset = -slideIndex * slideWidth;
-  slider.style.transform = `translateX(${offset}px)`;
-}
