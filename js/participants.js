81d87fc756b0cf5df26b6702125051d9fb0bb285
hayabusacosmos <hayabusacosmos@gmail.com>
2023-10-23 16:45:30 +0900

    出展団体 まだ途中ですが。

--- js/participants.js
+++ js/participants.js
@@ -8,22 +8,23 @@
   let contents = []; // 配列を用意
   for (let i=0; i<json.general.length; i++){
     contents.push(`
-      <div>
-        <div class="img"><img src="/img/projects/${json.general[i].id}/${json.general[i].id}_web_thumbnail.jpg" alt=""></div>
+      <div class="project">
+        <div class="project-img">
+          <img src="/img/projects/${json.general[i].id}/${json.general[i].id}_web_thumbnail.jpg" alt="">
+        </div>
         <div class="desc">
           <div class="project-name">${json.general[i].projectName[lang]}</div>
-          <div class="group-name">${json.general[i].groupName[lang]}</div>
-          <div class="place">
-            <div>${json.general[i].firstDayPlace[lang]}</div>
-            <div>${json.general[i].secondDayPlace[lang]}</div>
+          <div class="detail">
+            <div class="group-name">${json.general[i].groupName[lang]}</div>
+            <div class="place">${json.general[i].firstDayPlace[lang]}</div>
           </div>
         </div>
       </div>    
     `)
   }
   const contentsJoin = contents.join(""); // 配列の要素を連結して文字列とする
-
-  const exhibition = document.querySelector("#exhibition");
-
-  exhibition.insertAdjacentHTML("afterbegin", contentsJoin);
+  console.log(contentsJoin)
+  const exhibitionwrapper = document.querySelector("#exhibition > div");
+  console.log(exhibition)
+  exhibitionwrapper.insertAdjacentHTML("beforeend", contentsJoin);
 })()
